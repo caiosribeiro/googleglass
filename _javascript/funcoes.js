c63ca76f31291254200356702db48663e58df422
Caio Ribeiro <51218509+caiosribeiro@users.noreply.github.com>
2023-09-06 10:50:41 -0300

    Update funcoes.js

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,3 +1,11 @@
 function mudaFoto(foto) {
     document.getElementById("icone").src = foto;
 }
+
+function trocaFoto(foto) {
+    let conteudo = document.getElementById('conteudo')
+    
+    if (conteudo.offsetWidth < 742){
+        document.getElementById('mao').src = '_imagens/mao1.png'
+    }
+}
